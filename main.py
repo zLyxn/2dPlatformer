1e559fcc6d6147a6375f1a15e763c1a91c7329f8
Mika <79707824+zLyxn@users.noreply.github.com>
2024-03-11 08:40:58 +0100

    ini commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from typing import Any
+import pygame
+from threading import Timer
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+with open("./config.toml", "rb") as f:
+    data: dict[str, Any] = tomllib.load(f)
+for d in data:
+    globals()[d] = data.get(d)
+
+pygame.init()
+screen: pygame.display = pygame.display.set_mode()
+clock: pygame.time = pygame.time.Clock()
+dt: clock = None
+
+
+class Game:
+    def __init__(self) -> None:
+        self.isRunning: bool = True
+
+    def quit(self) -> None:
+        pygame.quit()
+        print("""
+             ____             _ 
+            | __ ) _   _  ___| |
+            |  _ \| | | |/ _ \ |
+            | |_) | |_| |  __/_|
+            |____/ \__, |\___(_)
+                   |___/        
+        """)
+        quit()
+
+
+frame: int = 0
+game: Game = Game()
+while game.isRunning:
+    dt: clock = clock.tick(60) / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game.isRunning = False
+
+    keys: pygame.key = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        game.isRunning = False
+
+        # HERE
+game.quit()
