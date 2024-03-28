0729d382bfe08bd54a09374bebee3c0a2714cf83
Jonathan <Jonathan.uloth@gmail.com>
2024-03-28 15:08:03 +0100

    Spieler fällt noch durch Blöcke

--- jonathan-Test.py
+++ jonathan-Test.py
@@ -16,7 +16,7 @@ LIGHT_BLUE = (173, 216, 230)
 PLAYER_WIDTH = 50
 PLAYER_HEIGHT = 50
 PLAYER_GRAVITY = 0.8
-PLAYER_JUMP = -15
+PLAYER_JUMP = -15  # evtl -9
 PLAYER_SPEED = 5
 
 coinCollected = False
@@ -29,13 +29,13 @@ pygame.display.set_caption("von Mika und Jonathan")
 BLOCK_SIZE = screen.get_width() / 16
 
 # default Position
-playerPosition = pygame.Vector2(4, 2)
+playerPosition = pygame.Vector2(5, 2)
 
 
 class Game:
     def __init__(self):
-        self.player = Player()
         self.level = Level()
+        self.player = Player(self.level)  # Übergebe eine Instanz des aktuellen Levels an den Spieler
 
     def game_loop(self):
         screen.fill(LIGHT_BLUE)
@@ -43,38 +43,66 @@ class Game:
         self.player.draw()
         pygame.display.flip()
 
+        self.player.update()
+        self.player_move()
+
     def player_move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w] and not self.player.is_block_above(self.level.blocks_group) and self.player.rect.top > 0:
             self.player.jump()  # Änderung: Spieler springen lassen, wenn W gedrückt wird
         if keys[pygame.K_a] and not self.player.is_block_left(self.level.blocks_group) and self.player.rect.left > 0:
             self.player.rect.x -= PLAYER_SPEED
-        if keys[pygame.K_d] and not self.player.is_block_right(self.level.blocks_group) and self.player.rect.right < screen.get_width():
+        if keys[pygame.K_d] and not self.player.is_block_right(
+                self.level.blocks_group) and self.player.rect.right < screen.get_width():
             self.player.rect.x += PLAYER_SPEED
 
 
 # Spieler-Klasse
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, level):
         super().__init__()
-        self.image = pygame.image.load("./assets/img/player.png").convert_alpha()  # Bild laden
-        self.image = pygame.transform.scale(self.image, (int(BLOCK_SIZE / 2), int(BLOCK_SIZE)))  # Größe ändern
-        self.rect = self.image.get_rect()  # Rect erstellen
-        self.rect.topleft = (BLOCK_SIZE * playerPosition.x, BLOCK_SIZE * playerPosition.y)  # Position
-        self.velocity_y = 0  # Vertikale Geschwindigkeit des Spielers
-        self.on_ground = False  # Variable zur Überprüfung, ob der Spieler auf dem Boden ist
+        self.image = pygame.image.load("./assets/img/player.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (int(BLOCK_SIZE / 2), int(BLOCK_SIZE)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (BLOCK_SIZE * playerPosition.x, BLOCK_SIZE * playerPosition.y)
+        self.velocity_y = 0
+        self.on_ground = False
+        self.level = level
 
     def update(self):
-        # Bewegung aktualisieren
         self.rect.y += self.velocity_y
 
-        # Spieler auf dem Boden halten
-        if self.rect.bottom >= screen.get_height():
+        # Überprüfen, ob der Spieler auf dem Boden steht oder auf einem Block steht
+        if self.is_block_below() or self.rect.bottom >= screen.get_height():
             self.rect.bottom = screen.get_height()
             self.on_ground = True
             self.velocity_y = 0
         else:
-            self.velocity_y += PLAYER_GRAVITY  # Schwerkraft anwenden, um nach unten zu ziehen
+            self.velocity_y += PLAYER_GRAVITY
+
+            # Wenn der Spieler nach unten fällt und auf einen Block trifft, passen Sie seine Position entsprechend an
+            if self.velocity_y > 0:
+                for block in self.level.blocks_group:
+                    if block.rect.colliderect(self.rect):
+                        if self.rect.bottom > block.rect.top and self.rect.top < block.rect.top:
+                            # Spieler fällt von oben auf den Block
+                            self.rect.bottom = block.rect.top
+                            self.on_ground = True
+                            self.velocity_y = 0
+                        elif self.rect.top < block.rect.bottom and self.rect.bottom > block.rect.bottom:
+                            # Spieler stößt von unten gegen den Block
+                            self.rect.top = block.rect.bottom
+                            self.velocity_y = 0
+                        else:
+                            # Standardkollision, seitlicher Zusammenstoß mit dem Block
+                            if self.velocity_y > 0:
+                                self.rect.bottom = block.rect.top
+                                self.on_ground = True
+                                self.velocity_y = 0
+                            else:
+                                self.rect.top = block.rect.bottom
+                                self.velocity_y = 0
+                        break
 
     def jump(self):
         if self.on_ground:  # Nur wenn der Spieler auf dem Boden ist
@@ -91,12 +119,22 @@ class Player(pygame.sprite.Sprite):
                 return True
         return False
 
-    def is_block_below(self, block_group):
-        player_rect = self.rect
-        for block in block_group:
-            if block.rect.top == player_rect.bottom and player_rect.left < block.rect.right and player_rect.right > block.rect.left:
+    def is_block_below(self):
+        player_rect = self.rect.copy()
+        player_rect.y += 1  # Verschiebung des Spielers nach unten um die Höhe eines Blocks
+        for block in self.level.blocks_group:
+            if block.rect.colliderect(player_rect):
+                # Setze die Y-Geschwindigkeit auf 0, wenn der Spieler auf einen Block trifft
+                self.velocity_y = 0
                 return True
         return False
+
+    # def is_block_below(self, block_group):
+    #    player_rect = self.rect
+    #    for block in block_group:
+    #        if block.rect.top == player_rect.bottom and player_rect.left < block.rect.right and player_rect.right > block.rect.left:
+    #            return True
+    #    return False
 
     def is_block_left(self, block_group):
         player_rect = self.rect
@@ -115,13 +153,13 @@ class Player(pygame.sprite.Sprite):
 
 class Level:
     def __init__(self):
-        self.blocks_group = pygame.sprite.Group()  # Erstelle eine Gruppe für die Blöcke
-        self.create_blocks()  # Erstelle die Blöcke
+        self.blocks_group = pygame.sprite.Group()
+        self.create_blocks()
 
     def create_blocks(self):
-        # Erstelle die Blöcke und füge sie der Gruppe hinzu
-        block_positions = [(x, 1) for x in range(1, 17)]  # Boden
-        block_positions.extend([(5, 3), (10, 2)])  # Zusatz Blöcke
+        block_positions = [(x + 1, 3) for x in range(4, 7)]
+        block_positions.extend([(x + 1, 2) for x in range(9, 12)])
+        block_positions.extend([(1, 1), (10, 2)])
         for block_pos in block_positions:
             block = Block(*block_pos)
             self.blocks_group.add(block)
@@ -145,16 +183,13 @@ game = Game()
 # Game-Loop
 running = True
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    game.player_move()  # Spielerbewegung aktualisieren
-    game.player.update()  # Spieler aktualisieren (z. B. für Schwerkraft)
-    game.game_loop()  # Spiel-Loop aktualisieren
 
-    # FPS
+    game.game_loop()
+
     pygame.time.Clock().tick(60)
 
 pygame.quit()
